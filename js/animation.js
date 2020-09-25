f668eaf6f05681e9c1da4409733d981636f04359
Kiimijeep <kimijeepa2@gmail.com>
2020-09-25 22:21:39 +0900

    jquery edit

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,8 @@
 $(window).scroll(function (){
     $('.scrollanime').each(function(){
         var elemPos = $(this).offset().top,
-        var scroll = $(window).scrollTop(),
-        var windowHeight = $(window).height();
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
           if (scroll > elemPos - windowHeight + 100){
               $(this).addClass("fadeInDown");
           }
