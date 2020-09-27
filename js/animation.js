3ebf7b6724906198a14fa58cfa8c18ea0ec76e0e
Kiimijeep <kimijeepa2@gmail.com>
2020-09-27 20:05:26 +0900

    animation edit

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,10 @@
-$(window).scroll(function (){
-    $('.scrollanime').each(function(){
-        var elemPos = $(this).offset().top,
-            scroll = $(window).scrollTop(),
-            windowHeight = $(window).height();
-          if (scroll > elemPos - windowHeight + 100){
-              $(this).addClass("fadeInDown");
-          }
+$(window).scroll(function () {
+    const wHeight = $(window).height();
+    const scrollAmount = $(window).scrollTop();
+    $('.scrollanime').each(function () {
+        const targetPosition = $(this).offset().top;
+        if(scrollAmount > targetPosition - wHeight + 60) {
+            $(this).addClass("fadeInDown");
+        }
     });
 });
